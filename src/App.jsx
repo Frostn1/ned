b90c50ffbaf6b404d0a344b938651fb93f747a37
sean <sean@lynxight.com>
2023-10-27 18:16:48 +0300

    Added a way to track monitors in pc and pass it to FE

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import "./App.css";
-import '@mantine/core/styles.css';
+import "./app.scss";
+// import '@mantine/core/styles.css';
 import { currentMonitor, availableMonitors } from '@tauri-apps/api/window';
 
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, createTheme } from '@mantine/core';
 import { invoke } from "@tauri-apps/api";
+import {useState} from 'react';
+
+const theme = createTheme({
+  fontFamily: 'Montserrat, sans-serif',
+  background: 'red',
+  defaultRadius: 'md',
+});
 
 function App() {
-  const monitor = availableMonitors();
+  // const monitor = availableMonitors();
+  const [monitors, setMonitors] = useState([]);
   async function getMonitors() {
-    await invoke("displayMonitors")
+    setMonitors(await invoke("display_monitors"))
   }
   return (
-    <MantineProvider>
-      Welcome to my app1
+    <div  id={'app'}>
+      Welcome to my app
       <button onClick={getMonitors}>
         Monitors
       </button>
-    </MantineProvider>
+      {JSON.stringify(monitors)}
+    </div>
   );
 }
 
