16eea8fab69c134d4d76b6fc34712b98adcbddd9
sean <sean@lynxight.com>
2023-10-28 00:43:48 +0300

    Finnally fixed moving displays

--- src/Components/Display/Display.jsx
+++ src/Components/Display/Display.jsx
@@ -1,15 +1,38 @@
 import React from 'react'
 import './Display.scss'
+import { PropTypes } from 'prop-types'
 
-const Display = ({}) => {
-  return (
-    <div id={'display'}>
-        Display
-    </div>
-  )
+const ratio = .1;
+
+const Display = ({ data, position, index, onMouseDown }) => {
+    const height = data.height * ratio;
+    const width = data.width * ratio;
+    const style = { width: `${width}px`, height: `${height}px`, top: `${position && position.y}px`, left: `${position && position.x || 0}px`}
+    console.log('position', position)
+    function onDragStart (e) {
+        console.log('drag start');
+        onMouseDown(true, index)
+    }
+    function onDragStop (e) {
+        console.log('drag stop');
+        onMouseDown(false, index)
+    }
+    return (
+        <div id={'display-'+index} key={Math.random()} style={style} onMouseDownCapture={onDragStart}>
+            <div className={'resolution'}>
+                {data.width} X {data.height}
+            </div>
+            <div className={'name'}>
+                {data.name}
+            </div>
+        </div>
+    )
 }
 
 Display.propTypes = {
-
+    data: PropTypes.object.isrequired,
+    position: PropTypes.object.isrequired,
+    index: PropTypes.number.isrequired,
+    onMouseDown: PropTypes.func.isrequired
 }
 export default Display
--- src/Components/DisplayContainer/DisplayContainer.jsx
+++ src/Components/DisplayContainer/DisplayContainer.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Display from '../Display/Display'
 import './DisplayContainer.scss'
-import {PropTypes} from 'prop-types';
+import { PropTypes } from 'prop-types'
+import { useState } from 'react';
 
-const DisplayContainer = ({monitors}) => {
-  return (
-    <div id={'display-container'}>
-        {monitors.map((mon) => <Display/>)}
-    </div>
-  )
+const DisplayContainer = ({ monitors }) => {
+
+    const [positions, setPositions] = useState(Array(monitors.length).fill({ x: 0, y: 0 }));
+    const [indexMoving, setIndexMoving] = useState(null);
+    function startMoveDisplay(state, index) {
+        // console.log("mouse location:", e.clientX, e.clientY);
+        setIndexMoving(state ? index : null);
+
+    }
+    function handleMoveDisplay(e) {
+        if (indexMoving == null) return;
+        console.log(e.clientX, e.clientY)
+        const image = document.getElementById('display-' + indexMoving)
+        const rect = image.getBoundingClientRect();
+        positions[indexMoving] = { x: e.clientX - rect.width / 2, y: e.clientY - rect.height };
+        setPositions([...positions]);
+    }
+
+    return (
+        <div id={'display-container'} onMouseMoveCapture={handleMoveDisplay} onClick={() => startMoveDisplay(false, null)}>
+            {monitors.map((mon, index) =>
+                <Display data={mon} position={positions[index]} index={index} onMouseDown={startMoveDisplay} />
+            )}
+        </div>
+    )
 }
 DisplayContainer.propTypes = {
-    monitors: PropTypes.object.isrequired
+    monitors: PropTypes.array.isrequired
 }
 export default DisplayContainer
